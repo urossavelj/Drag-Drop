a84ddaf07aa3369734287808e0602e94287f39df
Uroš Šavelj <uros.savelj@outlook.com>
2024-10-19 17:38:09 +0200

    extensions

---
+++ API/Extensions/Extensions.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+
+namespace API.Extensions
+{
+    public static class Extensions
+    {
+        public static async Task<string> GetRawBodyAsync(
+            this HttpRequest request,
+            Encoding encoding = null)
+        {
+            if (!request.Body.CanSeek)
+            {
+                // We only do this if the stream isn't *already* seekable,
+                // as EnableBuffering will create a new stream instance
+                // each time it's called
+                request.EnableBuffering();
+            }
+
+            request.Body.Position = 0;
+
+            var reader = new StreamReader(request.Body, encoding ?? Encoding.UTF8);
+
+            var body = await reader.ReadToEndAsync().ConfigureAwait(false);
+
+            request.Body.Position = 0;
+
+            return body;
+        }
+    }
+}
